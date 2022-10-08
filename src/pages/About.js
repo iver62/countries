0ce9185ce07554641717cf3a5d8c1cdff4485119
iver62 <deshayes.pierrick@hotmail.fr>
2022-10-08 16:37:33 +0200

    End of 1st part

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum iure, illum necessitatibus recusandae, mollitia perspiciatis eaque magni sit dignissimos odit ut doloremque molestias fugit neque id suscipit ipsam delectus. Eius quia ipsam soluta. Recusandae incidunt dignissimos unde tempora iusto sequi, quibusdam vitae, aspernatur reprehenderit ducimus neque? Blanditiis aspernatur praesentium animi? Odio aliquid assumenda velit perspiciatis! Eveniet illo maxime nobis illum aut hic fuga sint explicabo iure. Voluptatem ullam voluptates magni aliquam alias numquam quis minus quibusdam qui illo. Dolore, error voluptatem. Iste dolores suscipit, explicabo architecto rerum ullam numquam, nulla ratione recusandae voluptates magni unde illum, culpa vero ipsa neque.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda debitis quisquam fuga corrupti voluptas blanditiis atque enim, iusto natus omnis, nostrum fugit ipsum, rem iure dicta officia sapiente soluta. Molestias sint ipsa quaerat illo nulla quos odio corporis dolor numquam dignissimos? Accusantium dolores aliquam rerum, ullam aperiam quis laborum expedita eos odit, recusandae molestias vitae assumenda aut, pariatur natus fuga! Est, voluptatibus doloremque. Deserunt error distinctio debitis saepe ipsam fugit, fugiat, doloremque, earum dolorum laudantium vel eius perferendis expedita voluptas adipisci architecto iure cum officiis excepturi. Repellendus ullam, aperiam quas, voluptatum culpa autem alias fugit numquam natus aliquid, neque magnam doloremque. Beatae nihil tempore voluptatum distinctio minima pariatur cum numquam magni ab. Nihil praesentium optio fuga a perferendis eos ex deserunt alias cupiditate voluptas eligendi sequi nemo obcaecati dolores quis minima sunt omnis nulla rem, amet facilis dignissimos distinctio quas at. Asperiores saepe ducimus perferendis esse nobis illum mollitia accusantium? Accusantium quaerat corporis aliquid enim distinctio sapiente! Deserunt, id, hic fuga ab harum labore veritatis aperiam tempore optio dolores commodi, quo laboriosam accusamus quia. Culpa nostrum voluptatum voluptas iusto quam debitis! Odit veritatis, obcaecati quo maiores repellat eos tenetur enim quod quae veniam dolor vel cupiditate beatae blanditiis perferendis! Excepturi.</p>
+        </div>
+    );
+};
+
+export default About;
